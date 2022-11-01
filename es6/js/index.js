aea54b72638d7c0d00c83956e9d29ba9e48d336c
Joel Ross <joelross@uw.edu>
2022-11-01 12:21:58 -0700

    End of lecture demo

--- es6/js/index.js
+++ es6/js/index.js
@@ -8,3 +8,79 @@ const peopleArray = [
   {name: 'Emma', height: 71, weight: 152}
 ]
 console.log(peopleArray);
+
+
+// //what we have: [{name...}, {}, {}]
+// //what we want: ["Ada", "", ""]
+// function getNameStringFromPeopleObj(peopleObj) {
+//   const nameString = peopleObj.name;
+//   //return a name String
+//   return nameString;
+// }
+
+//                callback function
+const peopleNames = peopleArray.map(({name}) => {
+  const nameString = name;
+  //return a name String
+  return nameString;
+})
+console.log(peopleNames);
+
+peopleArray.map((peopleObj) => peopleObj.name);
+
+
+//calculate the average height/weight ratio
+const ratioTotal = peopleArray.reduce(
+  (oldRunningTotal, peopleObj, idx, array) => {
+    // console.log("oldTotal", oldRunningTotal);
+    const {height, weight} = peopleObj;
+
+    const ratio = height / weight;
+    // console.log("ratio", ratio);
+
+    const newRunningTotal = oldRunningTotal + ratio/array.length;
+    // console.log("newTotal", newRunningTotal);
+    return newRunningTotal;
+}, 0);
+console.log(ratioTotal);
+// console.log("average ratio", ratioTotal/peopleArray.length);
+
+
+const foo = (params, more) => 'foo '+params;
+
+console.log(foo("world"));
+
+const myArray = [1,2,3,4];
+// const x = myArray[0];
+// const y = myArray[1];
+// const z = myArray[2];
+const [x, y, ...theOthers] = myArray;
+console.log(x);
+console.log(y);
+console.log(theOthers);
+
+//destructuring objects
+// const myObject = {a: 1, b: 2};
+// const {b, a, c, } = myObject; //myObject.a to a, etc.
+// console.log(a); //=> 1
+// console.log(b); //=> 2;
+// console.log(c); //=> 3;
+
+
+// const personObj = {first: "Joel", last: "Ross"};
+
+// function getBMI( {height, weight} ) {
+
+  
+//   return 703*weight/(height*height);
+// }
+
+// getBMI(personObj)
+
+const person = {name: 'Ada', height: 64, weight: 135}
+
+const personWithHat = { hat: "baseball", ...person  }; //clone an object!
+console.log(personWithHat);
+
+const tallerAda = {...person, height:74 }
+console.log(tallerAda);
